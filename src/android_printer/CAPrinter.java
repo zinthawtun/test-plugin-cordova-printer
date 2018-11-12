797c8a1c58ee2aa5897ec4ba290cfcf867075115
Zin Thaw Tun <32751073+zinthawtun@users.noreply.github.com>
2018-11-12 19:32:51 +0000

    Update CAPrinter.java

--- src/android_printer/CAPrinter.java
+++ src/android_printer/CAPrinter.java
@@ -8,8 +8,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.example.util.PrintUtil;
-
 /**
  * This class echoes a string called from JavaScript.
  */
