0dee1d3b8b6c0b4f7173b73303725e200dd46933
zinthawtun <tun.zinthaw@gmail.com>
2018-11-13 13:07:20 +0000

    add

--- www/CAPrinter.js
+++ www/CAPrinter.js
@@ -2,4 +2,5 @@ var exec = require('cordova/exec');
 
 exports.fastPrint = function(arg0, success, error) {
     exec(success, error, 'CAPrinter', 'fastPrint', [arg0]);
+    window.alert('Hello');
 };
